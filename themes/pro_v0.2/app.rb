18652e2df03da2422d49f14e6b3c540127c0e46e
Aaron Addleman <aaddleman@frbsf.org>
2012-07-19 08:06:26 -0700

    added table of contents support and cleaned up content to stop producing errors. content still needs work

--- themes/pro_v0.2/app.rb
+++ themes/pro_v0.2/app.rb
@@ -4,6 +4,10 @@ require 'will_paginate'
 require 'will_paginate/array'
 require 'will_paginate/view_helpers/sinatra'
 
+# table of contents support
+require 'maruku'
+require 'nokogiri'
+
 # code coloring
 require "rack/pygments"
 
@@ -20,7 +24,20 @@ module Nesta
     
     helpers do
       # Add new helpers here.
-      
+      def can_generate_toc?
+        [:Maruku, :Nokogiri].all? { |cls| Object.const_defined?(cls) }
+      end
+
+      # Provide page TOC    
+      def toc(page, toc_template = :table_of_contents)
+        return nil unless can_generate_toc?
+        headings = Nokogiri::HTML(page.body(self)).css('h2')
+        toc_headers = headings.inject({}) do |mappings, header_node|
+          mappings[header_node.attr('id')] = header_node.content
+          mappings
+        end
+        haml(toc_template, :layout => false, :locals => { :toc_headers => toc_headers })
+      end
     end
 
     # Add new routes here.
